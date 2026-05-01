392213e9409b160186dc88532422a992181bbb5d
snehaborole5-ui <snehaborole5@gmail.com>
2026-05-01 16:53:05 +0530

    TODOS EVENT

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,60 @@
-let todosArr = [
-    {
-        todoItem: 'CSS',
-        todoId: f5d1
-    }
-]
+let todoArr =[ 
+         { 
+           todoId:'asdf123-123AS-21asd3',
+           todoItem:'CSS'
+         },
+
+        { 
+          todoId:'sdfg-123AS-21asd3',
+          todoItem:'JS & ES6'
+         },
+
+         { 
+          todoId:'zasasd23-123AS-21asd3',
+          todoItem:'JS and TS'
+         },
+] 
 
 
+const todoForm =document.getElementById('todoForm'); 
+const  todoContainer=document.getElementById('todoContainer') ;
+
+
+
+function createArr(arr){ 
+         
+       let result =' ';
+      
+    arr.forEach(ele=>{ 
+         result += `<li class="list-group-item d-flex justify-content-between" id="todoItem">
+                           <strong>${ele.todoItem}</strong> 
+                         <div>
+                             <i class="fa-solid fa-pen-to-square fa-2x  text-primary"></i>
+                             <i class="fa-solid fa-trash fa-2x text-danger "></i>   
+                         </div>
+                        </li>`    
+       })
+
+
+     todoContainer.innerHTML =result;
+
+}
+
+
+createArr(todoArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// todoContainer.addEventListener()
