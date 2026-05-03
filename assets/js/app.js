950281bfebfeac4da2af33a729c1ced4ba583a16
snehaborole5-ui <snehaborole5@gmail.com>
2026-05-03 12:57:11 +0530

    todos event update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,94 +1,136 @@
-let todoArr =[ 
-         { 
-           todoId:'asdf123-123AS-21asd3',
-           todoItem:'CSS'
-         },
-
-        { 
-          todoId:'sdfg-123AS-21asd3',
-          todoItem:'JS and ES6'
-         },
-
-         { 
-          todoId:'zasasd23-123AS-21asd3',
-          todoItem:'FLEX'
-         },
-] 
-
-
 const cl = console.log;
+
 const todoList = document.getElementById('todoList');
 const todoForm = document.getElementById('todoForm');
 const todoItemControl = document.getElementById('todoItem');
+const addTodoBtn = document.getElementById('addTodoBtn');
+const updateTodoBtn = document.getElementById('updateTodoBtn');
 
-const uuid = () => {
-  return String('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx').replace(
-    /[xy]/g,
-    character => {
-      const random = (Math.random() * 16) | 0
-      const value = character === 'x' ? random : (random & 0x3) | 0x8
-      return value.toString(16)
-    }
-  )
-}
+let EDIT_ID = null;
 
+// Schema
+let todoArr =[ 
+    { 
+        todoId:'asdf123-123AS-21asd3',
+        todoItem:'CSS'
+    },
+    { 
+        todoId:'sdfg-123AS-21asd3',
+        todoItem:'JS and ES6'
+    },
+    { 
+        todoId:'zasasd23-123AS-21asd3',
+        todoItem:'FLEX'
+    },
+] 
+
+// REMOVE
 function onRemove(ele){
-  let REMOVE_ID = ele.closest('li').id;
+    let REMOVE_ID = ele.closest('li').id;
 
-  let getIndex = todoArr.findIndex(todo =>{
-    return todo.todoId === REMOVE_ID
-  })
-  cl(getIndex)
+    let getIndex = todoArr.findIndex(todo =>{
+        return todo.todoId === REMOVE_ID
+    });
 
-  let removeTodo = todoArr.splice(getIndex,1);
-  ele.closest('li').remove();
-  Swal.fire({
-    title: `The Todo item ${removeTodo[0].todoItem} removed successfully !!!`,
-    timer: 3000,
-    icon:'success'
-})
+    let removedTodo = todoArr.splice(getIndex,1);
+    ele.closest('li').remove();
+
+    Swal.fire({
+        title: `The Todo item ${removedTodo[0].todoItem} removed successfully !!!`,
+        timer: 3000,
+        icon: 'success'
+    });
 }
 
+// EDIT
+function onEdit(ele){
+    EDIT_ID = ele.closest('li').id;
+
+    let EDIT_OBJ = todoArr.find(todo =>{
+        return todo.todoId === EDIT_ID
+    });
+
+    todoItemControl.value = EDIT_OBJ.todoItem;
+
+    addTodoBtn.classList.add('d-none');
+    updateTodoBtn.classList.remove('d-none');
+}
+
+// TEMPLATE
 function temmplating (arr){
     let result = '';
     arr.forEach(obj => {
         result += `<li class="list-group-item d-flex justify-content-between" id="${obj.todoId}">
-                           <strong>${obj.todoItem}</strong> 
-                         <div>
-                             <i class="fa-solid fa-pen-to-square fa-2x  text-primary role="button" "></i>
-                             <i class="fa-solid fa-trash fa-2x text-danger role="button" onclick="onRemove(this)"></i>   
-                         </div>
-                        </li> 
-        `
+                        <strong>${obj.todoItem}</strong>
+                        <div>
+                            <i class="fa-solid fa-pen-to-square fa-2x text-primary" role="button" onclick="onEdit(this)"></i>
+                            <i class="fa-solid fa-trash fa-2x text-danger" role="button" onclick="onRemove(this)"></i>   
+                        </div>
+                   </li>`;
     });
     todoList.innerHTML = result;
 }
 
 temmplating(todoArr)
 
+// ADD
 function onTodoSubmit(eve){
     eve.preventDefault();
+
     let newTodo = {
         todoItem : todoItemControl.value,
-        todoId : Date.now.toString()
+        todoId : Date.now().toString()
     }
+
     todoArr.push(newTodo);
 
     let li = document.createElement('li');
     li.className = 'list-group-item d-flex justify-content-between';
+    li.id = newTodo.todoId;
 
     li.innerHTML = `<strong>${newTodo.todoItem}</strong>
                     <div>
-                        <i class="fa-solid fa-pen-to-square fa-2x  text-primary "></i>
-                        <i class="fa-solid fa-trash fa-2x text-danger onclick="onRemove(this)"></i>   
+                        <i class="fa-solid fa-pen-to-square fa-2x text-primary" role="button" onclick="onEdit(this)"></i>
+                        <i class="fa-solid fa-trash fa-2x text-danger" role="button" onclick="onRemove(this)"></i>   
                     </div>`
 
-    todoList.append(li)
+    todoList.append(li);
     todoForm.reset();
+
     Swal.fire({
-        title: `The new Todo ${newTodo.todoItem} item added successfully !!!`,
+        title: `The new Todo ${newTodo.todoItem} added successfully !!!`,
         timer: 3000,
-    })
+        icon: 'success'
+    });
 }
 
-todoForm.addEventListener('submit', onTodoSubmit)
+// UPDATE
+function onTodoUpdate(){
+    let updatedValue = todoItemControl.value;
+
+    let getIndex = todoArr.findIndex(todo =>{
+        return todo.todoId === EDIT_ID
+    });
+
+    // update array
+    todoArr[getIndex].todoItem = updatedValue;
+
+    // update UI
+    let li = document.getElementById(EDIT_ID);
+    li.querySelector('strong').innerText = updatedValue;
+
+    // reset
+    todoForm.reset();
+    addTodoBtn.classList.remove('d-none');
+    updateTodoBtn.classList.add('d-none');
+
+    Swal.fire({
+        title: `Todo updated successfully !!!`,
+        timer: 3000,
+        icon: 'success'
+    });
+}
+
+// EVENTS
+todoForm.addEventListener('submit', onTodoSubmit);
+updateTodoBtn.addEventListener('click', onTodoUpdate);
